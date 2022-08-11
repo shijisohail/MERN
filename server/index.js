cb3bb3366365342daa49a244fdb4a5914a221dc3
shijisohail <shijisohail786@gmail.com>
2022-08-11 21:25:36 +0500

    check commit

--- server/index.js
+++ server/index.js
@@ -9,4 +9,7 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true})); // tasveery add kr
 app.use(cors());
 const CONNECTION_URL = 'mongodb+srv://mern:mern123@cluster0.0kti9zy.mongodb.net/?retryWrites=true&w=majority'; //atlas connection
 const PORT = process.env.PORT || 5000; // Port connection
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}); // Parameters are for erroes and Warnings
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true}) // Parameters are for erroes and Warnings but not necesary   
+.then(()=>app.listen(PORT,()=>console.log(`Server running on port : ${PORT}`)))// Agar Connection stable hua to ..
+.catch((error)=>console.log(error.message)); //Connection Not Successful
+//mongoose.set("useFindAndModify", false); //for not getting warning in console 
